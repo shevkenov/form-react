1bf8161918328b1bbc1c863cd1220fbbda6dd5d7
Shevkenov Andrey <shevkenov@gmail.com>
2021-03-25 19:23:39 +0200

    add useEffect, useRef

--- FE/src/hooks/DynamicForm.js
+++ FE/src/hooks/DynamicForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import AddIcon from '@material-ui/icons/Add';
 import PropTypes from 'prop-types';
 import Fab from "@material-ui/core/Fab";
@@ -15,21 +15,46 @@ const useStyles = makeStyles({
     },
 });
 
-const DynamicForm = ({ labels }) => {
+const getFieldElements = (_fields) => {
+    return (
+        _fields.map(field => <TextField
+            id={field}
+            label={field}
+            variant="outlined"
+            color="secondary"
+        />)
+    )
+}
 
+const DynamicForm = ({ labels, isExtendedForm }) => {
+    const [fields, setFields] = useState(labels);
+    const [fieldElements, setFieldElements] = useState(getFieldElements(fields))
+    const containerRef = useRef();
     const classes = useStyles();
+
+    useEffect(() => {
+        if(isExtendedForm ){
+            setFieldElements(getFieldElements(fields));
+        }else{
+            setFieldElements(getFieldElements(fields.slice(0,3)));
+        }
+    },[fields, isExtendedForm])
+
+    const inputFieldHandler = () => {
+        setFields([...fields, 'New field']);
+    }
+
+    const showHight = () => {
+        return containerRef.current ? containerRef.current.offsetHeight : '';
+    } 
 
     return (
         <>
-            <div className={classes.container}>
-                {labels.map(field => <TextField
-                    id={field}
-                    label={field}
-                    variant="outlined"
-                    color="secondary"
-                />)}
+            <div ref={containerRef} className={classes.container}>
+                {fieldElements}
+                <div>{showHight()}</div>
             </div>
-            <Fab color="secondary" aria-label="add">
+            <Fab color="secondary" aria-label="add" onClick={() => inputFieldHandler()}>
                 <AddIcon />
             </Fab>
         </>
